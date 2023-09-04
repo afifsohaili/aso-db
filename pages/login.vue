33effe70d378650425da531afa5bca4f55fdfb7a
Afif Sohaili (Ascenda) <afif.sohaili@kaligo.com>
2023-09-05 01:11:56 +0800

    Add logout link

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,15 @@
 <script setup>
+const user = computed(() => useSupabaseUser())
+const signout = () => useSupabaseClient().auth.signOut()
 </script>
 
 <template>
-  <login-form/>
+  <div v-if="user.value">
+    Already logged in
+
+    <button class="block" @click="signout">
+      Logout
+    </button>
+  </div>
+  <login-form v-else />
 </template>
