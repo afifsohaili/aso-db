14ca55d6b1327b915a401917c7dbd76a21d892f3
Afif Sohaili (Ascenda) <afif.sohaili@kaligo.com>
2025-02-26 17:12:41 +0800

    Remove Supabase configuration and related files

--- server/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import type { Database } from '~/supabase/types'
-
-export function initSupabaseAdmin(supabaseUrl: string, supabaseServiceRoleKey: string) {
-  return createClient<Database>(
-    supabaseUrl, supabaseServiceRoleKey,
-    { auth: { autoRefreshToken: false, persistSession: false } },
-  )
-}
